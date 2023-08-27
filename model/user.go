80e5315774b5b6956088784b1c3d447eb854163a
Alexander Kevin <alexander.kevin@shortlyst.ai>
2023-08-27 23:08:09 +0700

    Update model user validation

--- model/user.go
+++ model/user.go
@@ -2,6 +2,9 @@ package model
 
 import (
 	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 type User struct {
@@ -13,44 +16,19 @@ type User struct {
 	CreatedAt    *time.Time `json:"created_at"`
 }
 
-// func (u User) Validate() error {
-// 	return validation.ValidateStruct(
-// 		&u,
-// 		validation.Field(&u.PhoneNumber, validation.Required, validation.Length(10, 13), validation.Match(regexp.MustCompile(`^\+62\d+$`))),
-// 		validation.Field(&u.FullName, validation.Required, validation.Length(3, 60)),
-// 		validation.Field(&u.Password, validation.Length(6, 64)),
-// 	)
-// }
+func (u User) Validate() error {
+	return validation.ValidateStruct(
+		&u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.FullName, validation.Required, validation.Length(3, 60)),
+		validation.Field(&u.Password, validation.Length(6, 64)),
+	)
+}
 
-// func (u User) ValidateLogin() error {
-// 	return validation.ValidateStruct(
-// 		&u,
-// 		validation.Field(&u.PhoneNumber, validation.Required, validation.Length(10, 13), validation.Match(regexp.MustCompile(`^\+62\d+$`))),
-// 		validation.Field(&u.Password, validation.Required, validation.Length(6, 64)),
-// 	)
-// }
-
-// func IsPasswordValid(password string) bool {
-// 	if len(password) < 8 {
-// 		return false
-// 	}
-
-// 	hasUpperCase := false
-// 	hasNumber := false
-// 	hasSpecialChar := false
-
-// 	for _, char := range password {
-// 		if 'A' <= char && char <= 'Z' {
-// 			hasUpperCase = true
-// 		} else if '0' <= char && char <= '9' {
-// 			hasNumber = true
-// 		} else if !('a' <= char && char <= 'z') && !('A' <= char && char <= 'Z') && !('0' <= char && char <= '9') {
-// 			hasSpecialChar = true
-// 		}
-
-// 		if hasUpperCase && hasNumber && hasSpecialChar {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
+func (u User) ValidateLogin() error {
+	return validation.ValidateStruct(
+		&u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required, validation.Length(6, 64)),
+	)
+}
